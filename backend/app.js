8609b208c9a16bd677c2d30cd94349543649f21b
man3800 <man3801@gmail.com>
2024-01-14 21:06:47 +0900

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hi from Node js");
 });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
